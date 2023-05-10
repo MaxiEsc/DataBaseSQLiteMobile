166b7bb9bcad6f5a7af998feacf79301de789976
Escobar Maximiliano <maxescobarubp@gmail.com>
2023-05-09 22:31:29 -0300

    Actualizacion con la funcionalidad de eliminacion en el proyecto solo queda la funcionalidad de modificación

--- app/src/main/java/com/maxescobar/basededatos/MainActivity.java
+++ app/src/main/java/com/maxescobar/basededatos/MainActivity.java
@@ -19,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Lo de siempre
         etNombre = (EditText) findViewById(R.id.ettNombre);
         etCodigo = (EditText) findViewById(R.id.etnCodigo);
         etPrecio = (EditText) findViewById(R.id.etnPrecio);
-
     }
 
     //Metodo para insertar los productos
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         AdminiSQLite admin = new AdminiSQLite(this,"administracion", null,1);
         //Crea un objeto de tipoSQLiteDatabase para que este se escribible
         SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
-
         //Lo de siempre
         String codigo = etCodigo.getText().toString();
         String descripcion = etNombre.getText().toString();
@@ -73,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         String codigo = etCodigo.getText().toString();
 
         if (!codigo.isEmpty()) {
-            //Con la bvariable Cursor fingimos ser una operacion DDL de base de datos que nos permite utilizar la sentencia SELECT en la base de datos
+            //Con la variable Cursor fingimos ser una operacion DDL de base de datos que nos permite utilizar la sentencia SELECT en la base de datos
             Cursor fila = basededatos.rawQuery
                     ("SELECT descripcion, precio FROM articulo WHERE codigo =" + codigo, null);
             //Si lo encuentra entonces... moveToFirst() disparar un true
@@ -98,6 +97,38 @@ public class MainActivity extends AppCompatActivity {
             //si no esta compleo el codigo... pues que lo coloque
             Toast.makeText(this,"Debes escribir un codigo que desee buscar", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    //Metodo para Eliminar un producto
+    public void Eliminar(View vista) {
+        //Como arriba creamos un objeto de tipo AdminiSQLite que hereda de SQLiteOpenHelper que implementa metodos de base dedatos
+        AdminiSQLite admin = new AdminiSQLite(this, "administracion", null, 1);
+        //Aqui implementamos un objeto BasedeDatos(Objeto que actua como una base de datos simple) con el que implementaremos los metodos de la sqlite
+        SQLiteDatabase basededatos = admin.getWritableDatabase();
+
+        //Extraemos el codigo que necesitamos
+        String codigo = etCodigo.getText().toString();
+        //estructura condicional para controlar si el usuario ingresa codigo o no
+        if (!codigo.isEmpty()){
+            //En este caso este metodo devuelve un entero representando la cantidad de elementos borrados como JDBC.
+            int cantidad = basededatos.delete("articulos","codigo=" + codigo,null);
+            //Cerramos la base de datos
+            basededatos.close();
+            //limpiamos os campos
+            etPrecio.setText("");
+            etNombre.setText("");
+            etCodigo.setText("");
+            //Estructura condicional por si se ha elimnado el articulo o no
+            if (cantidad == 0){
+                //Mensaje para el usuario por si el objeto se elimino como corresponde
+                Toast.makeText(this,"El elemento ha sido eliminado correctamente",Toast.LENGTH_SHORT).show();
+            }else{
+                //Mensaje para el usuario por si el objeto no se ha eliminado por alguna razon
+                Toast.makeText(this,"El elemento no ha podido ser eliminado por que no existe",Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            //si no esta completo el codigo... pues que lo coloque
+            Toast.makeText(this,"Debes escribir un codigo que desee eliminar", Toast.LENGTH_SHORT).show();
+        }
     }
 }
